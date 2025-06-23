5fd8c2241c71e61bd808752c21a4b6346238d305
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-23 16:13:42 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/FileTree.fs
+++ lib/FileTree.fs
@@ -1,5 +1,6 @@
 module FileTree
 
+open System
 open System.IO
 
 /// Tree structure representing the file system
@@ -7,8 +8,8 @@ type FileTree =
     | File of string
     | Dir of string * FileTree list
 
-/// A filter function that takes a file path and returns whether it matches some condition
-type Filter = string -> bool
+// /// A filter function that takes a file path and returns whether it matches some condition
+// type Filter = string -> bool
 
 /// full path -> name only
 let filename (fd: string) : string = Path.GetFileName(fd)
@@ -16,24 +17,52 @@ let filename (fd: string) : string = Path.GetFileName(fd)
 let newFile (f: string) : FileTree = File(filename (f))
 
 /// ignored files
-let ignoreFiles: Filter =
-    fun f -> not (List.contains (filename (f)) [ ".gitignore" ])
+let ignoreFiles = fun f -> not (List.contains (filename (f)) [ ".gitignore" ])
 
 /// ignored dirs
-let ignoreDirs: Filter =
+let ignoreDirs =
     fun d -> not (List.contains (filename (d)) [ ".git"; "bin"; "tmp"; "ref"; "obj" ])
 
+
+/// fix error with `~` in path
+let tilde (path: string) : string =
+    if path.StartsWith("~") then
+        let home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+
+        if path.Length = 1 then
+            home
+        else
+            Path.Combine(home, path.Substring(2))
+    else
+        path
+
+
+/// dir depth limit
+let maxDepth = 15
+
 /// traverse file system forming tree from a given path
-let rec files (path: string) : FileTree =
+let rec files_ (path: string) (count: int) : FileTree =
+
+    if count > maxDepth then
+        failwith $"filetree depth overflow >{maxDepth}"
+
+    let path = tilde (path)
 
     let subfiles =
         Directory.GetFiles(path) |> Array.filter ignoreFiles |> Array.map newFile
 
     let subdirs =
-        Directory.GetDirectories(path) |> Array.filter ignoreDirs |> Array.map files
+        Directory.GetDirectories(path)
+        |> Array.filter ignoreDirs
+        |> Array.map (fun d -> files_ d (count + 1))
 
     Dir(filename path, List.ofArray (Array.append subfiles subdirs))
+
+let files (path: string) : FileTree = files_ path 0
+
 // files "."
+// files "~"
+// files "~/Espruino"
 
 /// filter F# projects (.xml)
 let fsproj: Filter = fun file -> file.EndsWith(".fsproj")
