b50867eda09635424bf285d06048533e78a0c835
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-23 13:34:34 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/FileTree.fs
+++ lib/FileTree.fs
@@ -5,8 +5,11 @@ open System.IO
 /// A filter function that takes a file path and returns whether it matches some condition
 type Filter = string -> bool
 
+/// filter F# projects (.xml)
+let proj: Filter = fun file -> file.EndsWith(".fsproj")
 
+/// file tree traversal with provided file names filter
 let rec traverse (path: string) (filter: Filter) =
     Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
 
-traverse "."
+// traverse "." proj
